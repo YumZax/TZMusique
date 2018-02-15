71488de53caddea79a10ebc8e5e6bf66993d7db8
= <maxime.louis@zenika.com>
2018-02-15 22:07:05 +0100

    Improve the script

--- scripts/process_midi_files.sh
+++ scripts/process_midi_files.sh
@@ -1,4 +1,10 @@
+if [ ! -d ../data/processed ]; then
+    mkdir ../data/processed
+fi
+
 for filename in ../data/*.mid
-do midicsv $filename >> $filename.csv
-mv ../data/*.csv ../data/processed
+    do midicsv $filename >> $filename.csv
+    mv ../data/*.csv ../data/processed
 done
+
+#TODO : Adapt those csv files to make them have simpler data

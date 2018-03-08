18a1d444d247495280f2590bc83221ac4cb85c69
= <maxime.louis@zenika.com>
2018-03-08 21:54:01 +0100

    Add requirements

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from midiutil import MIDIFile
+import numpy
+import pandas
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from lib.format_utils import *
 
-myMidi = MIDIFile(1)
-myMidi.readFile("data/Battletoads.mid")
+notes = processed_to_2d_array("./data/processed/beethoven_opus10_1.mid.csv.processed")
+
